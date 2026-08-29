2716ba166e9eb07d5756e9d544e08042ec57c1ab
Dozer <dozernapitupulu@gmail.com>
2026-08-29 13:59:09 +0700

    change footer widht

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -78,7 +78,7 @@ export function Footer({
 
   return (
     <footer className="border-t border-gray-200 bg-white">
-      <div className="mx-auto max-w-7xl px-4 py-10 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-9xl px-8 py-10 sm:px-8 lg:px-8">
         <div className="flex flex-col gap-6 sm:flex-row sm:items-start sm:justify-between">
           <div className="max-w-md">
             <FooterBrand />
